bf72ac1e924d48ef1baf625da643bd7f4c75968a
Jason Gagner <gagner.jason@pm.me>
2022-08-18 21:46:26 -0400

    Setup orleans client in API

---
+++ SpawnCloud.ChatService.API/Orleans/OrleansExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Orleans;
+
+namespace SpawnCloud.ChatService.API.Orleans;
+
+public static class OrleansExtensions
+{
+    public static WebApplicationBuilder UseOrleans(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton<ClusterClientHostedService>();
+        builder.Services.AddSingleton<IHostedService>(sp => sp.GetRequiredService<ClusterClientHostedService>());
+        builder.Services.AddSingleton<IClusterClient>(sp => sp.GetRequiredService<ClusterClientHostedService>().Client);
+        builder.Services.AddSingleton<IGrainFactory>(sp => sp.GetRequiredService<ClusterClientHostedService>().Client);
+
+        return builder;
+    }
+}
--- SpawnCloud.ChatService.API/Program.cs
+++ SpawnCloud.ChatService.API/Program.cs
@@ -1,4 +1,38 @@
+using Serilog;
+using SpawnCloud.ChatService.API.Orleans;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+    if (context.HostingEnvironment.IsDevelopment())
+    {
+        configuration.ReadFrom.Configuration(context.Configuration)
+            .Enrich.FromLogContext()
+            .WriteTo.Console();
+    }
+    else
+    {
+        // TODO: Determine which logging sink and configuration to use for production environments
+        throw new NotImplementedException();
+    }
+});
+builder.UseOrleans();
+
 var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHttpsRedirection();
+}
+
+app.UseSerilogRequestLogging();
+app.UseRouting();
+app.MapControllers();
 
 app.Run();
